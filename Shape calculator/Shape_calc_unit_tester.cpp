1adbd890a103e169669fde39ec8c5a6b23a3b355
kc400386 <93214571+kc400386@users.noreply.github.com>
2023-04-01 16:55:49 -0400

    Add files via upload
    Added the shape_calc_unit_tester_file to test new functions in. Also created a get function and receive function in the file, while adding the necessary includes the file may or may not need.

---
+++ Shape calculator/Shape_calc_unit_tester.cpp
@@ -0,0 +1,44 @@
+/**********************************************************
+ File: shape_calc_unit_tester
+ Class: CEN 3078, spring 2023
+ Author: Caleb Blackburn
+ Purpose: creating a unit tester for the shape calculator project
+**********************************************************/
+
+#include <iostream>
+#include "Rectangle.h" // includes the rectangle class in this file
+#include "Line.h" // includes the line class in this file
+#include "Circle.h" // includes the circle class in this file
+#include <vector>
+#include<limits>
+// above files may or may not be used for unit tests
+// below prototypes use template V as a placeholder for any datatype values
+template <typename V> V send_function();
+template <typename V> V receive_function(V value); // add more value where needed
+
+int main() {
+
+    return 0;
+
+}
+
+template <typename V> V send_function() {
+
+    V value_one;
+    V value_two;
+    V value_three;
+    //insert function to test here and add values where needed
+    return value_one;
+
+}
+
+template <typename V> void receive_function(V value) {
+
+    std::cout << value;
+    // add more couts where needed
+
+}
+
+
+
+
